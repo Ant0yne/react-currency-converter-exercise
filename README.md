549f316424502219e32b8e4fb067e876ab7f77d8
Ant0yne <antoine.soliv@gmail.com>
2024-03-22 09:29:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React exercise
+
+## Currency Converter
+
+A simple exercise to use form and input with React
+
+## To do
+
+-
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Footer from "./components/Footer";
+
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return <></>;
+}
+
+export default App;
